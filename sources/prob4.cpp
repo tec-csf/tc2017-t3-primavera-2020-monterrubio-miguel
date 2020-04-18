06145225d7b3e0d4372d93ac06c5e1c5b2e5b9b7
monterrubio-miguel <monterrubio.miguel@gmail.com>
2020-04-17 21:23:34 -0500

    completed first problem

--- sources/prob4.cpp
+++ sources/prob4.cpp
@@ -7,16 +7,81 @@
 
 #include <iostream>
 #include <vector>
+#include <algorithm>
+#include <stdlib.h>
+#include <time.h>
 
 using namespace std;
 
+vector<int> create(int size)
+{
+	srand (time(NULL));
+	vector<int> arr;
+	int x;
+	std::vector<int>::iterator it;
+
+	for (int i = 0; i < size; ++i)
+	{
+		
+		do {
+			x = rand() % 100 + 1;
+			it = find(arr.begin(), arr.end(), x);
+
+		} while (it != arr.end());
+
+		arr.push_back(x);
+	}
+
+	sort(arr.begin(), arr.end());
+
+	return arr;
+}
+
+void printArr(vector<int> arr)
+{
+	for (int i = 0; i < arr.size(); ++i)
+	{
+		cout << endl << arr[i];
+	}
+	cout << endl;
+}
+
+int searchIdx(vector<int> arr, int low, int high)
+{
+
+	if (high >= low)
+	{
+		int mid = (low + high) / 2;
+		if (arr[mid] == mid)
+		{
+			return mid;
+		}
+		else if (mid < arr[mid])
+		{
+			return searchIdx(arr, low, mid-1);
+		}
+		else if (mid > arr[mid])
+		{
+			return searchIdx(arr, mid+1, low);
+		}
+	}
+	else
+	{
+		return -1;
+	}
+}
+
 int main()
 {
-	vector<int> arr;
+	int size = 10;
+	
+	vector<int> arr = create(size);
+	printArr(arr);
 
-	for (int i = 0; i < count; ++i)
-	{
-		arr.push_back()
-	}
+	int i = searchIdx(arr, 0, size-1);
+
+	cout << endl << "indice i encontrado. a[" << i << "] =" << arr[i] << endl;
+
 	return 0;
 }
+
