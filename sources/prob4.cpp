d44f4e084968c12b0c5dc572d7cad7d28bc4f8bb
Miguel Monterrubio <monterrubio.miguel@gmail.com>
2020-04-17 19:20:19 -0500

    starting

---
+++ sources/prob4.cpp
@@ -0,0 +1,22 @@
+/*
+**Sea a[1..n] un vector ordenado de enteros todos distintos, implementar un algoritmo con la menor compejidad posible en el peor caso capaz de encontrar un índice i tal que 1≤i≤n y a[i] = i, suponiendo que tal índice exista.
+**
+**Algoritmo: Divide y vencerás
+**
+*/
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	vector<int> arr;
+
+	for (int i = 0; i < count; ++i)
+	{
+		arr.push_back()
+	}
+	return 0;
+}
